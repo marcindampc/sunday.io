a5730746b8690819e3737a15b73143239a6e4165
Marcin Dampc <marcin.dampc@marviq.com>
2020-07-10 08:44:55 +0200

    create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainFormComponent } from './components/main-form/main-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
